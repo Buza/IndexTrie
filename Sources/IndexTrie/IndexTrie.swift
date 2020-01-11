d9db90e199bca9f95e2bbf79ed7c76562a320fbd
buza <kylebuza@gmail.com>
2020-01-11 12:18:49 -0800

    Adjust internal class permission modifiers.

--- Sources/IndexTrie/IndexTrie.swift
+++ Sources/IndexTrie/IndexTrie.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public class IndexTrie<T : Comparable> {
 
-    class IndexTrieNode {
+    private class IndexTrieNode {
         var index                   : T?
         var childIndices            = Array<T>()
         var parent                  : IndexTrieNode?
